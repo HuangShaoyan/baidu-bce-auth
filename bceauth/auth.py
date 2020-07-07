576650156a44ac1135ddaef4beb2ccb0d3787f60
黄少彦 <huangshaoyan@ainemo.com>
2020-07-07 15:30:36 +0800

    按照 https://cloud.baidu.com/signature/index.html，修正了生成 CanonicalHeaders 和 signedHeaders 的方法

--- bceauth/auth.py
+++ bceauth/auth.py
@@ -3,7 +3,7 @@
 import hashlib
 import hmac
 from datetime import datetime
-from typing import List, Set
+from typing import List, Set, Tuple
 
 from .utils import uri_encode, uri_encode_except_slash
 
@@ -12,36 +12,37 @@ BCE_PREFIX: str = 'x-bce-'
 
 def make_auth(
     ak: str, sk: str, method: str, path: str,
-    params: dict, headers: dict
+    params: dict, headers: dict, headers_to_sign: Set[str] = None,
 ) -> str:
-    canonical_uri: str = uri_encode_except_slash(path)
-    canonical_query_string: str = _to_canonical_query_string(params)
-    canonical_headers: str = _to_canonical_headers(headers)
-    canonical_request: str = f'{method}\n{canonical_uri}\n{canonical_query_string}\n{canonical_headers}'  # noqa
+    canonical_uri = uri_encode_except_slash(path)
+    canonical_query_string = _to_canonical_query_string(params)
+    canonical_headers, signed_headers = _to_canonical_headers(
+        headers, headers_to_sign)
+    canonical_request = f'{method}\n{canonical_uri}\n{canonical_query_string}\n{canonical_headers}'  # noqa
 
-    timestamp: str = _to_timestamp()
+    timestamp = _to_timestamp()
 
-    auth_string_prefix: str = f'bce-auth-v1/{ak}/{timestamp}/1800'
+    auth_string_prefix = f'bce-auth-v1/{ak}/{timestamp}/1800'
 
-    signing_key: str = hmac.new(
+    signing_key = hmac.new(
         sk.encode(),
         auth_string_prefix.encode(),
         hashlib.sha256).hexdigest()
 
-    signature: str = hmac.new(
+    signature = hmac.new(
         signing_key.encode(),
         canonical_request.encode(),
         hashlib.sha256).hexdigest()
 
-    return f'bce-auth-v1/{ak}/{timestamp}/1800//{signature}'
+    return f'bce-auth-v1/{ak}/{timestamp}/1800/{signed_headers}/{signature}'
 
 
 def _to_canonical_query_string(params: dict) -> str:
     param_list: List[str] = []
     for k, v in params.items():
-        new_k: str = uri_encode(k)
+        new_k = uri_encode(k)
         if v:
-            new_v: str = uri_encode(str(v))
+            new_v = uri_encode(str(v))
         else:
             new_v = ''
         param_list.append(f'{new_k}={new_v}')
@@ -50,27 +51,32 @@ def _to_canonical_query_string(params: dict) -> str:
 
 def _to_canonical_headers(
     headers: dict, headers_to_sign: Set[str] = None
-) -> str:
+) -> Tuple[str, str]:
     headers = headers or {}
 
     if headers_to_sign is None or len(headers_to_sign) == 0:
         headers_to_sign = {
+            # 百度云只强制要求编码 "host" header
             'host',
-            'content-md5',
-            'content-length',
-            'content-type',
+        }
+    else:
+        headers_to_sign = {
+            h.strip().lower()
+            for h in headers_to_sign
         }
 
     result: List[str] = []
+    signed_headers: Set[str] = set()
     for k, v in headers.items():
-        k_lower: str = k.strip().lower()
+        k_lower = k.strip().lower()
 
         if k_lower.startswith(BCE_PREFIX) or k_lower in headers_to_sign:
-            new_k: str = uri_encode(k_lower)
-            new_v: str = uri_encode(str(v).strip())
+            new_k = uri_encode(k_lower)
+            new_v = uri_encode(str(v).strip())
             result.append(f'{new_k}:{new_v}')
+            signed_headers.add(new_k)
 
-    return '\n'.join(sorted(result))
+    return '\n'.join(sorted(result)), ';'.join(sorted(signed_headers))
 
 
 def _to_timestamp() -> str:
